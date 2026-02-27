6876bd2257691a29abbf481aab9935e8ad8d59ec
Nathan Verzemnieks <njvrzm@gmail.com>
2026-02-27 17:03:26 +0100

    Frontend externalization: build tooling, vendored utilities

--- jest-setup.js
+++ jest-setup.js
@@ -1,2 +1,16 @@
-// Jest setup provided by Grafana scaffolding
+import React from 'react';
 import './.config/jest-setup';
+import { matchers } from './src/test/matchers';
+
+global.React = React;
+
+const mockIntersectionObserver = jest.fn().mockImplementation((callback) => ({
+  observe: jest.fn().mockImplementation((elem) => {
+    callback([{ target: elem, isIntersecting: true }]);
+  }),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
+global.IntersectionObserver = mockIntersectionObserver;
+
+expect.extend(matchers);
